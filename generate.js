245b27124e8011c6d07a0cbc1f5161ad9664d195
Otubusin Ademuyiwa (Innovate. Educate. Elevate.) <53128146+tay4real@users.noreply.github.com>
2026-05-20 13:21:07 +0100

    update favicon links to use absolute URLs

--- generate.js
+++ generate.js
@@ -316,7 +316,7 @@ function generateIndexPage(posts) {
   <meta name="description" content="Insights on Nigerian tech, transportation, entrepreneurship, and product development from Otubusin Ademuyiwa Solutions Ltd." />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+  <link rel="icon" type="image/svg+xml" href="https://oassolutions.com.ng/favicon.svg" />
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" />
   <style>
     ${getBaseStyles()}
@@ -443,7 +443,7 @@ function generatePostPage(post, blocks) {
   <meta property="og:url" content="https://blog.oassolutions.com.ng/posts/${slug}.html" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link rel="icon" type="image/svg+xml" href="../favicon.svg" />
+  <link rel="icon" type="image/svg+xml" href="https://oassolutions.com.ng/favicon.ico" />
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" />
   <style>
     ${getBaseStyles()}
